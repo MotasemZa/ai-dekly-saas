091be08fced2570b88c2d6b6b81dfccf7ee7cb16
Motasem Zaitoun <85106848+MotasemZa@users.noreply.github.com>
2025-06-04 02:27:36 +0300

    Fix Dompdf installation

--- public_html/download_invoice.php
+++ public_html/download_invoice.php
@@ -2,7 +2,7 @@
 session_start();
 require_once 'config.php';
 require_once 'classes/Invoice.php';
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 use Dompdf\Dompdf;
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
